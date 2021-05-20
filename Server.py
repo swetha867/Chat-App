2947e261d3bd9ce0d240e8a7f56379ba85a3ca94
ZhuoJunHe <38018284+ZhuoJunHe@users.noreply.github.com>
2021-05-19 23:02:39 -0700

    multi threading added
    multi threading added

--- Server.py
+++ Server.py
@@ -4,8 +4,11 @@ import sys
 import socket
 import time
 import threading
+
+
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
 
 database = {
     "Swetha": {
@@ -27,7 +30,7 @@ database = {
 
         ]
     },
-     "Bob": {
+    "Bob": {
         "password": "123",
         "messages": [
 
@@ -39,10 +42,10 @@ database = {
 def handle_authentication(conn):
     conn.sendall(b'Welcome! Please log in.')
 
-    # Using this sleep to prevent bundling 
+    # Using this sleep to prevent bundling
     # Welcome banner and username prompt.
-    time.sleep(1)   
-    
+    time.sleep(1)
+
     conn.sendall(b'Username:')
     while True:
         username = conn.recv(1024)
@@ -51,7 +54,7 @@ def handle_authentication(conn):
             break
         else:
             print(username.decode())
-    
+
     time.sleep(1)
 
     conn.sendall(b'Password:')
@@ -68,7 +71,7 @@ def handle_authentication(conn):
     else:
         print("Login Failure for : "+username)
         conn.sendall(b'Login Failure')
-    
+
     time.sleep(1)
 
 def handle_user_list(conn):
@@ -111,41 +114,52 @@ def handle_user_inbox(conn, user_name):
         messages_string = header + messages_formatted + footer
     conn.sendall(messages_string.encode())
 
+
+def handle_client(conn, addr):
+    print('Socket connection established successfully  :', addr)
+    try:
+        handle_authentication(conn)
+        with conn:
+            while True:
+                option = conn.recv(1024)
+                if option is None:
+                    continue
+                else:
+                    option = option.decode()
+                    if option == '1':
+                        handle_user_list(conn)
+                    elif option == '2':
+                        user_name = wait_for_message_from_client(conn)
+                        time.sleep(1)
+                        message = wait_for_message_from_client(conn)
+                        global database
+                        database.get(user_name,{}).get("messages",[]).append(message)
+                    elif option == '3':
+                        user_name = wait_for_message_from_client(conn)
+                        handle_user_inbox(conn, user_name)
+                    elif option == '7':
+                        print("Closing connection..")
+                        break
+    except Exception:
+        print("Unexpected error:", sys.exc_info())
+        conn.close()
+       # s.close()
+   # print("Connection closed from :", addr)
+
+
+
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
+        print(f"[LISTENING] Server is listening on {HOST}")
         while True:
             conn, addr = s.accept()
-            print('Socket connection established successfully  :', addr)
-            try:
-                handle_authentication(conn)
-                with conn:
-                    while True:
-                        option = conn.recv(1024)
-                        if option is None:
-                            continue
-                        else:
-                            option = option.decode()
-                            if option == '1':
-                                handle_user_list(conn)
-                            elif option == '2':
-                                user_name = wait_for_message_from_client(conn)
-                                time.sleep(1)
-                                message = wait_for_message_from_client(conn)
-                                global database
-                                database.get(user_name,{}).get("messages",[]).append(message)
-                            elif option == '3':
-                                user_name = wait_for_message_from_client(conn)
-                                handle_user_inbox(conn, user_name)
-                            elif option == '7':
-                                print("Closing connection..")
-                                break
-            except Exception:
-                print("Unexpected error:", sys.exc_info())
-                conn.close()
-                s.close()
-            print("Connection closed from :", addr)
+            thread = threading.Thread(target=handle_client, args=(conn, addr))
+            thread.start()
+            print(f"[Active Connections] {threading.activeCount() -1}")
+
+
 
 if __name__ == "__main__":
     main()
